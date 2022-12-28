0500b74079a68f2b86f21e8594617244b5c8eb83
Eoin Bathurst <silent@silentecho.eu>
2022-12-28 17:33:59 +0100

    misc prep for sandbox update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import type { PluginOption, UserConfig } from "vite";
+
+const reloadSVGandCSS = (): PluginOption => {
+  return {
+    name: "reload-svg",
+    enforce: "post",
+    handleHotUpdate({ file, server }) {
+      if (file.endsWith(".svg") || file.endsWith(".css")) {
+        console.log("reloading svg file...");
+
+        server.ws.send({
+          type: "full-reload",
+          path: "*",
+        });
+      }
+    },
+  };
+};
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), reloadSVGandCSS()],
 };
 
 export default config;
