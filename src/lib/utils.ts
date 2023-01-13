2a423c039e262aa8f4c75a39d89567c8cfd41605
Eoin Bathurst <silent@silentecho.eu>
2023-01-13 23:27:07 +0100

    default merge fn

---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+import { default as merge } from "ts-deepmerge";
+import type { z } from "zod";
+
+type IObject = Record<string, any>;
+
+export const defaultMerge =
+  <T extends IObject>(guard: (obj: any) => z.SafeParseReturnType<T, T>) =>
+  (a: T, b: any) => {
+    if (typeof b === "object") {
+      const merged = merge(a, b);
+      const parsed = guard(merged);
+      if (parsed.success) {
+        return parsed;
+      } else {
+        console.error("Invalid type for merge", merged, parsed);
+      }
+    }
+    return guard(a);
+  };
