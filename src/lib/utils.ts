d2be49aa66416d6370b997ebe55452b9100a71c4
Eoin Bathurst <silent@silentecho.eu>
2023-01-13 23:47:42 +0100

    more specific typing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,14 +5,14 @@ type IObject = Record<string, any>;
 
 export const defaultMerge =
   <T extends IObject>(guard: (obj: any) => z.SafeParseReturnType<T, T>) =>
-  (a: T, b: any) => {
+  (a: T, b: any): z.SafeParseReturnType<T, T> => {
     if (typeof b === "object") {
       const merged = merge(a, b);
       const parsed = guard(merged);
       if (parsed.success) {
         return parsed;
       } else {
-        console.error("Invalid type for merge", merged, parsed);
+        console.error("Invalid type for merge", { a, b, merged, parsed });
       }
     }
     return guard(a);
