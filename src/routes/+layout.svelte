ff342aa94a3d15e7c3160f3171ffbc66c81071ca
Eoin Bathurst <silent@silentecho.eu>
2023-04-09 19:51:30 +0200

    add footer and favicon

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { clipboard } from "@svelteuidev/composables";
+
+  let copied = false;
+
+  const onCopy = () => {
+    copied = true;
+    setTimeout(() => (copied = false), 1200);
+  };
+</script>
+
+<div class="container">
+  <slot>??? Contact <i class="fa-brands fa-discord" /> SilentEcho#0001</slot>
+
+  <div class="footer">
+    <a href="http://preview.charalayouts.com">
+      <i class="fa-solid fa-circle-chevron-right" /> Check out the Preview</a>
+    <a href="https://github.com/SilentEchoGM/charachorder-layouts/issues">
+      <i class="fa-brands fa-github" /> Report an Issue</a>
+    |
+    <span>Contact the Developer:</span>
+    <a href="http://www.twitter.com/SilentEchoGM">
+      <i class="fa-brands fa-twitter" /> @SilentEchoGM</a>
+
+    <button use:clipboard={"SilentEcho#0001"} on:click={onCopy}
+      ><i class="fa-brands fa-discord" />
+      {#if copied}
+        Copied!{:else}SilentEcho#0001{/if}</button>
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    min-height: calc(100dvh - 16px);
+    min-width: calc(100vh - 16px);
+    box-sizing: border-box;
+  }
+  .footer {
+    justify-content: center;
+    display: flex;
+    gap: 1rem;
+    margin-top: auto;
+    color: rgb(138, 138, 138);
+  }
+
+  a,
+  button {
+    all: unset;
+    cursor: pointer;
+  }
+</style>
